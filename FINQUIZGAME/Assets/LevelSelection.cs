d9ddacd33b065afc19f55c8a2d61cea1afb8ef13
NoobMaster <bk201_perez@yahoo.com>
2021-04-22 19:38:11 +0800

    commit

--- FINQUIZGAME/Assets/LevelSelection.cs
+++ FINQUIZGAME/Assets/LevelSelection.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class LevelSelection : MonoBehaviour
+public class  : MonoBehaviour
 {
 
     private int levelReached;
